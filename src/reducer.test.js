7cdd4266fdc63ab05deb5ec1984e19e86323c6f7
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2025-03-04 16:23:35 +0000

    Implement exercise 6.1

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -9,7 +9,6 @@ describe('unicafe reducer', () => {
   }
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
     const action = {
       type: 'DO_NOTHING'
     }
@@ -31,5 +30,58 @@ describe('unicafe reducer', () => {
       ok: 0,
       bad: 0
     })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('counterReducer is a pure function', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const inputState = initialState
+    const inputStateCopy = { ...inputState }
+    deepFreeze(inputState)
+
+    const outputState1 = counterReducer(inputState, action)
+    const outputState2 = counterReducer(inputState, action)
+
+    // If there is a change in the output state...
+    expect(inputState).not.toEqual(outputState1)
+
+    // ...the input state is not changed
+    expect(inputState).toEqual(inputStateCopy)
+    
+    // Always returns the same response when called with the same parameter
+    expect(outputState1).toEqual(outputState2)
+    expect(outputState1).toEqual({ good: 1, ok: 0, bad: 0 })
+    expect(outputState2).toEqual({ good: 1, ok: 0, bad: 0 })
   })
 })
